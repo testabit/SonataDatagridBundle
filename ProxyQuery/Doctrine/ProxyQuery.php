114a51485512bbcbbd2a478d1d3a840a5f6c49a6
Romain Mouillard <romain.mouillard@gmail.com>
2014-05-19 16:10:46 +0200

    Fix pager query when using leftJoin

--- ProxyQuery/Doctrine/ProxyQuery.php
+++ ProxyQuery/Doctrine/ProxyQuery.php
@@ -12,6 +12,7 @@
 namespace Sonata\DatagridBundle\ProxyQuery\Doctrine;
 
 use Doctrine\ORM\Query;
+use Doctrine\ORM\QueryBuilder;
 use Sonata\DatagridBundle\ProxyQuery\BaseProxyQuery;
 use Sonata\DatagridBundle\ProxyQuery\ProxyQueryInterface;
 
@@ -27,21 +28,82 @@ class ProxyQuery extends BaseProxyQuery implements ProxyQueryInterface
      */
     public function execute(array $params = array(), $hydrationMode = null)
     {
-        $query = $this->queryBuilder->getQuery();
+        // Limit & offset
+        $this->queryBuilder->setMaxResults($this->getMaxResults());
+        $this->queryBuilder->setFirstResult($this->getFirstResult());
 
         // Sorted field and sort order
-        $sortBy = $this->getSortBy();
+        $sortBy    = $this->getSortBy();
         $sortOrder = $this->getSortOrder();
 
         if ($sortBy && $sortOrder) {
-            $sortBy = sprintf('%s.%s', $query->getRootAlias(), $sortBy);
-            $query->orderBy($sortBy, $sortOrder);
+            $rootAliases = $this->queryBuilder->getRootAliases();
+            $rootAlias   = $rootAliases[0];
+            $sortBy      = sprintf('%s.%s', $rootAlias, $sortBy);
+
+            $this->queryBuilder->orderBy($sortBy, $sortOrder);
         }
 
-        // Limit & offset
-        $query->setFirstResult($this->getFirstResult());
-        $query->setMaxResults($this->getMaxResults());
+        return $this->getFixedQueryBuilder($this->queryBuilder)->getQuery()->execute($params, $hydrationMode);
+    }
 
-        return $query->execute();
+    /**
+     * This method alters the query to return a clean set of object with a working
+     * set of Object
+     *
+     * @param QueryBuilder $queryBuilder
+     *
+     * @return QueryBuilder
+     */
+    private function getFixedQueryBuilder(QueryBuilder $queryBuilder)
+    {
+        $queryBuilderId = clone $queryBuilder;
+
+        // step 1 : retrieve the targeted class
+        $from  = $queryBuilderId->getDQLPart('from');
+        $class = $from[0]->getFrom();
+
+        // step 2 : retrieve the column id
+        $idName = current($queryBuilderId->getEntityManager()->getMetadataFactory()->getMetadataFor($class)->getIdentifierFieldNames());
+
+        // step 3 : retrieve the different subjects id
+        $rootAliases = $queryBuilderId->getRootAliases();
+        $rootAlias   = $rootAliases[0];
+        $select      = sprintf('%s.%s', $rootAlias, $idName);
+        $queryBuilderId->resetDQLPart('select');
+        $queryBuilderId->add('select', 'DISTINCT ' . $select);
+
+        // for SELECT DISTINCT, ORDER BY expressions must appear in select list
+        /* Consider
+            SELECT DISTINCT x FROM tab ORDER BY y;
+        For any particular x-value in the table there might be many different y
+        values.  Which one will you use to sort that x-value in the output?
+        */
+        // todo : check how doctrine behave, potential SQL injection here ...
+        if ($this->getSortBy()) {
+            $sortBy = $this->getSortBy();
+            if (strpos($sortBy, '.') === false) {
+                // add the current alias
+                $sortBy = $rootAlias . '.' . $sortBy;
+            }
+            $sortBy .= ' AS __order_by';
+            $queryBuilderId->addSelect($sortBy);
+        }
+
+        $results    = $queryBuilderId->getQuery()->execute(array(), Query::HYDRATE_ARRAY);
+        $idx        = array();
+        $connection = $queryBuilder->getEntityManager()->getConnection();
+        foreach ($results as $id) {
+            $idx[] = $connection->quote($id[$idName]);
+        }
+
+        // step 4 : alter the query to match the targeted ids
+        if (count($idx) > 0) {
+            $queryBuilder->andWhere(sprintf('%s IN (%s)', $select, implode(',', $idx)));
+            $queryBuilder->setMaxResults(null);
+            $queryBuilder->setFirstResult(null);
+        }
+
+        return $queryBuilder;
     }
 }
